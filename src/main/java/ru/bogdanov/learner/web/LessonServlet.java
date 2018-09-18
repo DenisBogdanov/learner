47dd4366a6cb6938d8c18b5525c693f9dc932e2c
Denis Bogdanov <bogdanov.math@gmail.com>
2018-09-18 22:51:13 +0300

    Use LessonServlet as a front-controller.

--- src/main/java/ru/bogdanov/learner/web/LessonServlet.java
+++ src/main/java/ru/bogdanov/learner/web/LessonServlet.java
@@ -2,10 +2,13 @@ package ru.bogdanov.learner.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.bogdanov.learner.model.Lesson;
 import ru.bogdanov.learner.repository.LessonRepository;
 import ru.bogdanov.learner.repository.mock.InMemoryLessonRepositoryImpl;
 import ru.bogdanov.learner.util.LessonUtil;
+import ru.bogdanov.learner.web.lesson.LessonRestController;
 import ru.bogdanov.learner.web.user.SecurityUtil;
 
 import javax.servlet.ServletException;
@@ -21,13 +24,20 @@ import java.util.Objects;
  * Denis, 16.09.2018
  */
 public class LessonServlet extends HttpServlet {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LessonServlet.class);
 
-    private LessonRepository repository;
+    private ConfigurableApplicationContext springContext;
+    private LessonRestController lessonController;
 
     @Override
     public void init() throws ServletException {
-        repository = new InMemoryLessonRepositoryImpl();
+        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        lessonController = springContext.getBean(LessonRestController.class);
+    }
+
+    @Override
+    public void destroy() {
+        springContext.close();
+        super.destroy();
     }
 
     @Override
@@ -37,21 +47,20 @@ public class LessonServlet extends HttpServlet {
         switch (action == null ? "getAll" : action) {
             case "delete":
                 int id = getId(request);
-                LOGGER.info("Delete {}", id);
-                repository.delete(id, SecurityUtil.authUserId());
+                lessonController.delete(id);
                 response.sendRedirect("lessons");
                 break;
             case "create":
             case "update":
                 final Lesson lesson = "create".equals(action) ?
                         new Lesson(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 30)
-                        : repository.get(getId(request), SecurityUtil.authUserId());
+                        : lessonController.get(getId(request));
                 request.setAttribute("lesson", lesson);
                 request.getRequestDispatcher("/lesson-form.jsp").forward(request, response);
                 break;
             case "getAll":
-                LOGGER.info("getAll");
-                request.setAttribute("lessons", LessonUtil.getWithGoal(repository.getAll(SecurityUtil.authUserId()), LessonUtil.DEFAULT_DAILY_GOAL));
+            default:
+                request.setAttribute("lessons", lessonController.getAll());
                 request.getRequestDispatcher("/lessons.jsp").forward(request, response);
                 break;
         }
@@ -60,15 +69,16 @@ public class LessonServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        String id = request.getParameter("id");
 
-        Lesson lesson = new Lesson(id.isEmpty() ? null : Integer.parseInt(id),
-                LocalDateTime.parse(request.getParameter("startDateTime")),
+        Lesson lesson = new Lesson(LocalDateTime.parse(request.getParameter("startDateTime")),
                 request.getParameter("description"),
                 Integer.parseInt(request.getParameter("duration")));
 
-        LOGGER.info(lesson.isNew() ? "Create {}" : "Update {}", lesson);
-        repository.save(lesson, SecurityUtil.authUserId());
+        if (request.getParameter("id").isEmpty()) {
+            lessonController.create(lesson);
+        } else {
+            lessonController.update(lesson, getId(request));
+        }
         response.sendRedirect("lessons");
     }
 
