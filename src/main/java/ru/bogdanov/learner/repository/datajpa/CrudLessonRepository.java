b6170c66e7b2394d459bef74fbf47a1bcc3a8c81
Denis Bogdanov <bogdanov.math@gmail.com>
2018-09-22 18:13:51 +0300

    Add Spring Data JPA implementation for Lesson repository.

--- src/main/java/ru/bogdanov/learner/repository/datajpa/CrudLessonRepository.java
+++ src/main/java/ru/bogdanov/learner/repository/datajpa/CrudLessonRepository.java
@@ -1,10 +1,37 @@
 package ru.bogdanov.learner.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.bogdanov.learner.model.Lesson;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Denis, 22.09.2018
  */
+@Transactional(readOnly = true)
 public interface CrudLessonRepository extends JpaRepository<Lesson, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Lesson l WHERE l.id=:id AND l.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Override
+    @Transactional
+    Lesson save(Lesson lesson);
+
+    @Query("SELECT l FROM Lesson l WHERE l.user.id=:userId ORDER BY l.startDateTime DESC")
+    List<Lesson> getAll(@Param("userId") int userId);
+
+    @SuppressWarnings("JpaQlInspection")
+    @Query("SELECT l FROM Lesson l WHERE l.user.id=:userId AND l.startDateTime BETWEEN :startDateTime AND :endDateTime ORDER BY l.startDateTime DESC")
+    List<Lesson> getBetween(@Param("startDateTime") LocalDateTime startDateTime,
+                            @Param("endDateTime") LocalDateTime endDateTime,
+                            @Param("userId") int userId);
+
 }
--- src/main/java/ru/bogdanov/learner/repository/datajpa/DataJpaLessonRepositoryImpl.java
+++ src/main/java/ru/bogdanov/learner/repository/datajpa/DataJpaLessonRepositoryImpl.java
@@ -2,6 +2,7 @@ package ru.bogdanov.learner.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.bogdanov.learner.model.Lesson;
 import ru.bogdanov.learner.repository.LessonRepository;
 
@@ -15,30 +16,40 @@ import java.util.List;
 public class DataJpaLessonRepositoryImpl implements LessonRepository {
 
     @Autowired
-    private CrudLessonRepository crudRepository;
+    private CrudLessonRepository crudLessonRepository;
+
+    @Autowired
+    private CrudUserRepository crudUserRepository;
 
     @Override
+    @Transactional
     public Lesson save(Lesson lesson, int userId) {
-        return null;
+        if (lesson.isNew() && get(lesson.getId(), userId) == null) {
+            return null;
+        }
+        lesson.setUser(crudUserRepository.getOne(userId));
+        return crudLessonRepository.save(lesson);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudLessonRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Lesson get(int id, int userId) {
-        return null;
+        return crudLessonRepository.findById(id)
+                .filter(lesson -> lesson.getUser().getId() == userId)
+                .orElse(null);
     }
 
     @Override
     public List<Lesson> getAll(int userId) {
-        return null;
+        return crudLessonRepository.getAll(userId);
     }
 
     @Override
     public List<Lesson> getBetween(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudLessonRepository.getBetween(startDateTime, endDateTime, userId);
     }
 }
